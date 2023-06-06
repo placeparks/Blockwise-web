9a6485b405281510c186a288305cd7b62fd56093
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-07 00:23:51 +0500

    Add files via upload

---
+++ src/components/FifthPage/FAQ.js
@@ -0,0 +1,40 @@
+export default function FAQ(){
+    return(
+        <>
+        <h1 style={{fontWeight:"700"}}>Frequently Asked Questions</h1>
+        <div className="card mb-3" style={{maxWidth: "540px;", border:"none", backgroundColor:"transparent"}}>
+  <div className="row g-0">
+    <div className="col-md-3 pt-3">
+      <img src='/Metaverse/MetaGirl.png' className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body" style={{textAlign:"left"}}>
+      <h5 className="card-title">(1) How is Decentralized finance better than traditional finance?</h5>
+<p className="card-text">In a traditional finance system, the power is controlled by one central authority while limiting access and control to users. However, decentralized finance is equipped with:<br/>
+- Bottom-up decision-making and management <br/>
+- Accurate and smart-contract-powered decisions <br/>
+- Trust backed by cryptography and blockchain technology <br/>
+- Users enjoy equal voting right for making protocol changes <br/>
+- Utmost transparency and completely public <br/>
+- Completely global <br/>
+- Low operational costs</p>
+<h5 className="card-title">(2) What will be the cost of starting my DeFi project?</h5>
+<p className="card-text">The actual cost for a DeFi platform development project is totally based on the functionalities, business objectives, and technology stack chosen by the client. All these requirements are discussed with the DeFi development company you choose and then the final pricing model is prepared.</p>
+<h5 className="card-title">(3) How do I make money with DeFi?</h5>
+<p className="card-text">When it comes to DeFi development, there are ample opportunities to earn passive income: <br/>
+- Become a liquidity provider <br/>
+- Staking <br/>
+- Lending <br/>
+- Yield farming <br/>
+- Depositing crypto for an APY</p>
+<h5 className="card-title">(4) What is a DeFi staking pool?</h5>
+<p className="card-text">A DeFi staking pool enables multiple stakeholders in merging their computational resources which helps them in maximizing the chances of being rewarded.</p>
+<h5 className="card-title">(5) How to make money with DeFi yield farming?</h5>
+<p className="card-text">Individuals engaged in DeFi yield farming calculate their returns in terms of annual percentage yields (APY) while opening doors to profitable results and innovative ways to earn passive income.</p>
+      </div>
+    </div>
+  </div>
+</div>
+        </>
+    )
+}
