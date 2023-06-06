3ddb124f0b1d9b4e96cac8da7d12cc70a7804020
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-06 19:51:15 +0500

    Update FAQ.js

--- src/components/SecondPage/FAQ.js
+++ src/components/SecondPage/FAQ.js
@@ -9,9 +9,9 @@ export default function FAQ(){
     </div>
     <div className="col-md-8">
       <div className="card-body" style={{textAlign:"left"}}>      
-        <h5 className="card-title" style={{fontWeight:"700"}}>(1) How can Blockwise tech help you with Web 3.0 development?</h5>
+        <h5 className="card-title">(1) How can Blockwise tech help you with Web 3.0 development?</h5>
                 <p className="card-text">At Smart Blockchain Tech, we are equipped with a team of subject matter experts and experienced blockchain engineers who can develop and deliver any and every Web 3.0-based solution in line with your business requirements. In addition, we provide business-oriented consulting solutions to help you accomplish your goals</p>
-            <h5 className="card-title" style={{fontWeight:"700"}}>(2) How can businesses benefit from Web 3.0 development?</h5>
+            <h5 className="card-title">(2) How can businesses benefit from Web 3.0 development?</h5>
             <p className="card-text">Businesses can leverage the following benefits by investing in Web 3.0 development:,<br/>⦁ Streamlined business processes due to smart contracts and distributed ledger<br/>
 ⦁ Reduced cost due to the elimination of third-party service providers<br/>
 ⦁ Higher transparency in business operations<br/>
@@ -20,20 +20,20 @@ export default function FAQ(){
 ⦁ Cheaper and faster access to data<br/>
 ⦁ More efficient supply chains<br/>
 ⦁ Cheap and flexible infrastructure</p>
-<h5 className="card-title" style={{fontWeight:"700"}}>(3) What are the core features of Web 3.0?</h5>
+<h5 className="card-title">(3) What are the core features of Web 3.0?</h5>
 <p className="card-text">⦁ Decentralization: Data is stored on the blockchain<br/>
 ⦁ Autonomous: Smart contracts are self-executing<br/>
 ⦁ Permissionless: No restrictions on accessibility<br/>
 ⦁ Censorship resistance: The content available on Web 3.0 cannot be removed or stopped<br/>
 ⦁ Secure messaging: Messages cannot be hacked<br/>
 ⦁ Fraud protection: Tamper-free transactions</p>
-<h5 className="card-title" style={{fontWeight:"700"}}>(4) How can businesses gain an essential competitive edge with Web 3.0 development?</h5>
+<h5 className="card-title">(4) How can businesses gain an essential competitive edge with Web 3.0 development?</h5>
 <p className="card-text">Web 3.0 development can empower businesses with the following:<br/>
 ⦁ Improved trust by returning data ownership to web users<br/>
 ⦁ Better onboarding and user journeys<br/>
 ⦁ Better customer engagement<br/>
 ⦁ Opportunities for innovation, thereby providing ingenious solutions to customers</p>
-<h5 className="card-title" style={{fontWeight:"700"}}>(5) How soon can Blockwise tech start with the development of my project?</h5>
+<h5 className="card-title">(5) How soon can Blockwise tech start with the development of my project?</h5>
 <p className="card-text">Our services are focused on accelerating your time-to-market. Once you educate us about your needs, we will create a roadmap to accelerate your development journey to help you quickly tap into the market and gain an essential competitive edge.</p>
       </div>
     </div>
