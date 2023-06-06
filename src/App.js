b404cfce75a23464b2f9842cabc5d6c541a34b65
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-06 05:16:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import FirstPage from './components/FirstPage';
-
+import SecondPage from './components/SecondPage';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MdFirstPage } from 'react-icons/md';
@@ -10,12 +10,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar/>
 
         <Routes>
           <Route exact path='/' element={<FirstPage />} />
           <Route exact path='/firstPage' element={<FirstPage />} />
-          <Route exact path='/secondPage' element={""}/>
+          <Route exact path='/secondPage' element={<SecondPage/>}/>
           <Route exact path='/thirdPage' element={""}/>
           <Route exact path='/forthPage' element={""}/>
           <Route exact path='/fifthPage' element={""}/>
