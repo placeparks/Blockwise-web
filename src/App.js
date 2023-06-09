102b63ece2b4c24eb14fb5f8e9bb6a0615d17c95
placeparks <86215469+placeparks@users.noreply.github.com>
2023-06-09 17:07:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import FirstPage from './components/FirstPage';
-import SecondPage from './components/SecondPage';
-import ThirdPage from './components/ThirdPage';
-import ForthPage from './components/ForthPage';
-import FifthPage from './components/FifthPage';
-import SixthPage from './components/SixthPage';
-import SeventhPage from './components/SeventhPage';
-import EighthPage from './components/EighthPage';
-import NinethPage from './components/NinethPage';
-import TenthPage from './components/TenthPage';
+import Main from './components/Main';
+import Web3 from './components/Web3';
+import MetaversePage from './components/MetaversePage';
+import NFT from './components/NFT';
+import DefiProtocol from './components/DefiProtocol';
+import ExchangePage from './components/ExchangePage';
+import Wallets from './components/Wallets';
+import Blockchains from './components/Bloackchains';
+import CoinDevelopment from './components/CoinDevelopment';
+import Blog from './components/Blog';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MdFirstPage } from 'react-icons/md';
 
@@ -20,17 +20,17 @@ function App() {
         <Navbar/>
 
         <Routes>
-          <Route exact path='/' element={<FirstPage />} />
-          <Route exact path='/firstPage' element={<FirstPage />} />
-          <Route exact path='/secondPage' element={<SecondPage/>}/>
-          <Route exact path='/thirdPage' element={<ThirdPage/>}/>
-          <Route exact path='/forthPage' element={<ForthPage/>}/>
-          <Route exact path='/fifthPage' element={<FifthPage/>}/>
-          <Route exact path='/sixthPage' element={<SixthPage/>}/>
-          <Route exact path='/seventhPage' element={<SeventhPage/>}/>
-          <Route exact path='/eighthPage' element={<EighthPage/>}/>
-             <Route exact path='/ninethPage' element={<NinethPage/>}/>
-                <Route exact path='/tenthPage' element={<TenthPage/>}/>
+          <Route exact path='/' element={<Main />} />
+          <Route exact path='/main' element={<Main />} />
+          <Route exact path='/web3' element={<Web3/>}/>
+          <Route exact path='/metaversePage' element={<MetaversePage/>}/>
+          <Route exact path='/nft' element={<NFT/>}/>
+          <Route exact path='/defiProtocol' element={<DefiProtocol/>}/>
+          <Route exact path='/exchangePage' element={<ExchangePage/>}/>
+          <Route exact path='/wallets' element={<Wallets/>}/>
+          <Route exact path='/blockchains' element={<Blockchains/>}/>
+          <Route exact path='/coinDevelopment' element={<CoinDevelopment/>}/>
+          <Route exact path='/blog' element={<Blog/>}/>
         </Routes>
       </Router>
     </div>
